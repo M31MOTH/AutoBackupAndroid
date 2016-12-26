7862b1b88932f25d576356255907c6e3901bad0b
Triple-eM <andrea.morzenti99@gmail.com>
2016-12-26 11:47:26 +0100

    Add files via upload

---
+++ ABA_Manual_Version/V 1.0.2/Changelog.md
@@ -0,0 +1,19 @@
+V 1.0.2
+
+- Added the feature tu start ADB as root (backup and restore /~ directory)
+
+V 1.0.1
+
+- Added the possibility to be redirected to the browser at XDA Developer for Super Slim ADB
+  Drivers download by @Snoop05 (all merits to him for his work).
+
+V 1.0.0
+
+ - Initlial release
+ - Possibility to backup from the phone and restore to the phone:
+  - Photos (DCIM Folder)
+  - Music
+  - Bluetooth
+  - WhatsAPP
+  - All the file on your Internal Memory (/storage/emulated/0)
+ - A green-colored output will shown when the process is finished
---
+++ ABA_Manual_Version/V 1.0.2/auto_backup_android.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+#include<dos.h>
+
+using namespace std;
+int main()
+{
+  string ip, choose, answer1, answer2;
+  int select;
+  cout<<"Do you have ADB Driver installed on your pc? (if you don't konw the asnswer insert 'no'): ";
+  cin>>answer1;
+  cout<<"Do you want to use ADB as root? ";
+  cin>>answer2;
+  if (answer1=="no")
+  {
+    cout<<"Press enter to open the download page for the driver (This is not my work, all Thanks to @Snoop05)"<<endl;
+    system("pause");
+    system("start chrome.exe http://forum.xda-developers.com/showthread.php?t=2588979");
+  }
+  else
+  {
+    cout<<"Insert the IP address of your device: ";
+    cin>>ip;
+    system(("adb connect "+ip).c_str());
+    if(answer2="yes"||answer2=="Yes")
+    {
+      system("adb root");
+    }
+    cout<<"Choose if you want to BACKUP or RESTORE file: ";
+    cin>>choose;
+    if(choose=="BACKUP"||choose=="Backup"||choose=="backup")
+    {
+  	   cout<<"Enter the number that corresponds to what you want to save: "<<endl
+       <<"1. Photos (DCIM)"<<endl<<"2. Music"<<endl<<"3. Bluetooth"<<endl
+       <<"4. WhastApp (all)"<<endl<<"5. All SDCard"<<endl;
+       cin>>select;
+       if(select==1)
+       {
+         system("adb pull /storage/emulated/0/DCIM /Backup/Photos");
+         system("cls");
+         system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        if(select==2)
+        {
+          system("adb pull /storage/emulated/0/Music /Backup/Music");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        if(select==3)
+        {
+          system("adb pull /storage/emulated/0/bluetooth /Backup/Bluetooth");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        if(select==4)
+        {
+          system("adb pull /storage/emulated/0/WhastApp /Backup/WhastApp");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        if(select==5)
+        {
+          system("adb pull /storage/emulated/0 /Backup/Complete");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+      }
+      if(choose=="Restore"||choose=="RESTORE"||choose=="restore")
+      {
+        cout<<"Enter the number that corresponds to what you want to restore: "<<endl
+        <<"1. Photos (DCIM)"<<endl<<"2. Music"<<endl<<"3. Bluetooth"<<endl
+        <<"4. WhastApp (all)"<<endl<<"5. All SDCard"<<endl;
+        cin>>select;
+        if(select==1)
+        {
+          system("adb push /Backup/Photos /storage/emulated/0/DCIM");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        if(select==2)
+        {
+          system("adb push /Backup/Music /storage/emulated/0/Music");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        if(select==3)
+        {
+          system("adb push /Backup/Bluetooth /storage/emulated/0/Bluetooth");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        if(select==4)
+        {
+          system("adb push /Backup/WhastApp /storage/emulated/0/WhastApp");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        if(select==5)
+        {
+          system("adb push /Backup/Complete /storage/emulated/0");
+          system("cls");
+          system("color 0A");
+          cout<<"Complete";
+          return 0;
+        }
+        return 0;
+      }
+    }
+}
